1bd8ce085e2766412a4612d2d9d5e120a7cc3ea0
Varun Mehta <varun.sagi20@gmail.com>
2019-04-09 19:38:06 +0530

    logging in file server.log

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const morgan = require('morgan')
+const fs = require('fs')
+const path = require('path')
 
 const app = express()
 
 // Apply Middleware
 app.use(morgan('common'))
+
+// Setup logging a file (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'server.log'), {
+	flags: 'a'
+})
+app.use(morgan('combined', { stream: accessLogStream }))
 
 app.get('/', (_, res) => {
 	res.json({ hello: 'world' })
