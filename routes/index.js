e1ef546a8824311654c9a737ef02e80e4ef22257
Varun Mehta <varun.sagi20@gmail.com>
2019-04-11 20:17:41 +0530

    404 page added

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
+/* global __dirname */
+const path = require('path')
+
 const homeRoute = require('./home')
 const productRoutes = require('./products')
 
 /* Remember order matters when registering routes */
-const pageNotFoundTemplate = () => '<h2>404 - Page Not Found</h2>'
-
 module.exports = app => {
 	app.use(homeRoute)
 	app.use('/product', productRoutes)
 
-	app.use((_, res, next) => {
-		res.status(404).send(pageNotFoundTemplate())
-		next()
+	// Setting up 404 message
+	app.use((_, res) => {
+		res.sendFile(path.join(__dirname, '../', 'views', '404.html'))
 	})
 }
