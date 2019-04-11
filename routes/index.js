6f0ecc4c09dddb11fc1377f578032c20dfd2233d
Varun Mehta <varun.sagi20@gmail.com>
2019-04-11 18:59:34 +0530

    comment about route order

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const home = require('./home')
 const product = require('./products')
 
+/* Remember order matters when registering routes */
 module.exports = app => {
 	app.use(home)
 	app.use(product)
