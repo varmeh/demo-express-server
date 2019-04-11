131cdc1d8101f26f62ed662f7fcd8df10602fe88
Varun Mehta <varun.sagi20@gmail.com>
2019-04-11 19:08:46 +0530

    404 handling

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,14 @@ const home = require('./home')
 const product = require('./products')
 
 /* Remember order matters when registering routes */
+const pageNotFoundTemplate = () => '<h2>404 - Page Not Found</h2>'
+
 module.exports = app => {
 	app.use(home)
 	app.use(product)
+
+	app.use((_, res, next) => {
+		res.status(404).send(pageNotFoundTemplate())
+		next()
+	})
 }
