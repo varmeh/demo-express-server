05bac77580b4e61c798b4c2de85dd56224728d27
Varun Mehta <varun.sagi20@gmail.com>
2019-04-13 15:37:02 +0530

    home template parametrization

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,18 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (_, res) => {
-	res.render('home')
+	res.render('home', {
+		pageTitle: 'Home',
+		prods: [
+			{
+				title: 'Time Flies',
+				image: '',
+				type: 'Books',
+				price: '9.99$',
+				description: 'A great book'
+			}
+		]
+	})
 })
 
 module.exports = router
