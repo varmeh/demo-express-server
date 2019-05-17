7d1812d019326ef7e467f1f33794b7683a5fcbbd
Varun Mehta <varun.sagi20@gmail.com>
2019-05-17 23:54:21 +0530

    configure db info

--- app/util/database.js
+++ app/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2')
+// Sequelize/index required for js intellisense
+const Sequelize = require('sequelize/index')
 
-const pool = mysql.createPool({
-	host: 'localhost',
-	user: 'root',
-	password: 'password',
-	database: 'node-complete'
+/* Export Connection Pool */
+module.exports = new Sequelize('node-complete', 'root', 'password', {
+	dialect: 'mysql',
+	host: 'localhost'
 })
-
-module.exports = pool.promise()
