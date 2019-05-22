486d1691dfbc3cb7c039ba7d8d6528b40906ea56
Varun Mehta <varun.sagi20@gmail.com>
2019-05-22 13:00:06 +0530

    database change to mongodb

--- app/util/database.js
+++ app/util/database.js
@@ -1,8 +1,31 @@
-// Sequelize/index required for js intellisense
-const Sequelize = require('sequelize/index')
+const mongodb = require('mongodb')
 
-/* Export Connection Pool */
-module.exports = new Sequelize('node-complete', 'root', 'password', {
-	dialect: 'mysql',
-	host: 'localhost'
-})
+/**
+ * @param {function} callback - Success callback
+ * @param {function} error - error callback
+ */
+
+let _db
+
+exports.mongoConnect = (callback, error) => {
+	mongodb.MongoClient.connect(
+		'mongodb+srv://service-account:lrO2JByKvwH6W9am@cluster0-free-mumbai-hsmgc.mongodb.net/shop?retryWrites=true',
+		{ useNewUrlParser: true }
+	)
+		.then(client => {
+			console.log('db connection: SUCCESS :', client)
+			_db = client.db()
+			callback()
+		})
+		.catch(err => {
+			console.log('db connection: FAILED : ', err)
+			error(err)
+		})
+}
+
+exports.getdb = () => {
+	if (_db) {
+		return _db
+	}
+	throw 'No database found'
+}
