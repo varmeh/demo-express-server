141f7a7c9fc429ddc741e3d779ecac95246dd9e4
Varun Mehta <varun.sagi20@gmail.com>
2019-05-28 17:35:29 +0530

    session variable added

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -2,7 +2,7 @@ exports.getLogin = (_, res) => {
 	res.render('auth/login', { pageTitle: 'Login' })
 }
 
-exports.postLogin = (_, res) => {
-	res.setHeader('Set-Cookie', 'loggedIn=true')
+exports.postLogin = (req, res) => {
+	req.session.isLoggedIn = true
 	res.redirect('/')
 }
